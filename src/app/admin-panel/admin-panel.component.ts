6759c8b8b88a8d4f405145f7df16b36c8e557b26
Zdravko Kolev <zdrawko.kolew@gmail.com>
2024-10-15 19:36:45 +0300

    NG Conf Demo initial version

---
+++ src/app/admin-panel/admin-panel.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IGridEditDoneEventArgs, IGX_GRID_ACTION_STRIP_DIRECTIVES, IGX_GRID_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_SELECT_DIRECTIVES, IgxRadioComponent, IgxRadioGroupDirective, IRowDataEventArgs } from 'igniteui-angular';
+import { Subject, takeUntil } from 'rxjs';
+import { CategoryModel } from '../models/ng-conf-toy-store-api/category-model';
+import { ToyModel } from '../models/my-api/toy-model';
+import { NgConfToyStoreAPIService } from '../services/ng-conf-toy-store-api.service';
+import { MyAPIService } from '../services/my-api.service';
+
+@Component({
+  selector: 'app-admin-panel',
+  standalone: true,
+  imports: [IGX_GRID_ACTION_STRIP_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_GRID_DIRECTIVES, IgxRadioGroupDirective, IgxRadioComponent, FormsModule],
+  templateUrl: './admin-panel.component.html',
+  styleUrls: ['./admin-panel.component.scss']
+})
+export class AdminPanelComponent implements OnInit, OnDestroy {
+  private destroy$: Subject<void> = new Subject<void>();
+  public myAPIToyModel: ToyModel[] = [];
+  public selectVisible: boolean = false;
+  public ngConfToyStoreAPICategoryModel: CategoryModel[] = [];
+
+  constructor(
+    private myAPIService: MyAPIService,
+    private ngConfToyStoreAPIService: NgConfToyStoreAPIService,
+  ) {}
+
+  ngOnInit() {
+    this.myAPIService.getToyModelList1().pipe(takeUntil(this.destroy$)).subscribe(
+      data => this.myAPIToyModel = data
+    );
+    this.ngConfToyStoreAPIService.getCategoryModelList().pipe(takeUntil(this.destroy$)).subscribe(
+      data => this.ngConfToyStoreAPICategoryModel = data
+    );
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  public apiToysCreateToyRowAdded(args: IRowDataEventArgs) {
+    this.myAPIService.postToyModel(args.rowData).pipe(takeUntil(this.destroy$)).subscribe(
+      _ => this.myAPIService.getToyModelList1().pipe(takeUntil(this.destroy$)).subscribe(data => this.myAPIToyModel = data)
+    );
+  }
+
+  public apiToysDeleteToyByIdRowDeleted(args: IRowDataEventArgs) {
+    this.myAPIService.deleteToyModel(args.rowKey).pipe(takeUntil(this.destroy$)).subscribe(
+      _ => this.myAPIService.getToyModelList1().pipe(takeUntil(this.destroy$)).subscribe(data => this.myAPIToyModel = data)
+    );
+  }
+
+  public apiToysUpdateToyRowEditDone(args: IGridEditDoneEventArgs) {
+    if(args.isAddRow == false) {
+      this.myAPIService.putToyModel(args.rowData).pipe(takeUntil(this.destroy$)).subscribe(
+        _ => this.myAPIService.getToyModelList1().pipe(takeUntil(this.destroy$)).subscribe(data => this.myAPIToyModel = data)
+      );
+    }
+  }
+}
